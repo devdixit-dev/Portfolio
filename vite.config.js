82cafd504339715b17607334ae8c8c411b736fb5
Dev Dixit <devdixitsocial@gmail.com>
2025-05-14 08:49:58 +0530

    config updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import 'dotenv/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT | 3000
-  }
+  base: '/'
 })
